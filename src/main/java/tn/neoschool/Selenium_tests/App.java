770fcbe8dffd02f84b2dea7a6d08e0d0025f90c7
chhaibinidhal-cell <chhaibinidhal@gmail.com>
2026-09-18 11:56:33 +0100

    first commit

---
+++ src/main/java/tn/neoschool/Selenium_tests/App.java
@@ -0,0 +1,10 @@
+package tn.neoschool.Selenium_tests;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
